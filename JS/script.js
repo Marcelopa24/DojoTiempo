6f377a770465e9d9d43a170396873e2b1fd0d6f9
Marcelopa24 <marcelopa94@hotmail.com>
2022-06-23 18:33:23 -0500

    nuevo code

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,32 @@ function loadingw(city){
     var elementh2 =document.querySelector(".sanjose h2");
     elementh2.innerHTML= city; 
 }
+
+
+const switchTemp = document.querySelector(".temp")
+
+
+    switchTemp.addEventListener('change', (event) => {
+        const currentTemp = event.target
+         console.log(currentTemp)
+     });
+
+function convert(){
+    
+
+    var temps =document.querySelectorAll(".tempmax, .tempmin");
+    /*var FoC = elementselect.value;*/
+    for(const idx in temps){
+        
+        let farenheite =(parseInt (temps[idx].innerHTML) * 9/5)+32;
+        temps[idx].innerHTML=farenheite;
+    }
+    /*if(var temps =document.querySelectorAll(".tempmax, .tempmin");){
+        temps[idx].innerHTML=farenheite
+    }
+    else{
+        nome.innerHTML="Jane Doe"
+    }
+    }*/
+}
+
